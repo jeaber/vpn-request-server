596dbbf1f1f7368b0254175cd7ec027597ea82ad
Jeaber <jeaber@gmail.com>
2021-05-25 16:03:48 -0500

    update to use oldest vpn first

--- src/project_file.ts
+++ src/project_file.ts
@@ -1,19 +1,25 @@
 import Axios, { AxiosRequestConfig } from "axios";
 
-// export const VPNS = ['http://192.168.1.90:9292', 'http://192.168.1.91:9292', 'http://192.168.1.92:9292', 'http://192.168.1.93:9292', 'http://192.168.1.80:9292', 'http://192.168.1.81:9292']; // 
-// export const VPNS_FREE = ['http://192.168.1.80:9292', 'http://192.168.1.81:9292', 'http://192.168.1.82:9292']; // 
-export const VPNS = ['http://192.168.1.80:9494']; // 
-export const VPNS_FREE = ['http://192.168.1.80:9494']; // 
+export const VPNS = ['http://192.168.1.90:9494', 'http://192.168.1.91:9494', 'http://192.168.1.92:9494', 'http://192.168.1.93:9494', 'http://192.168.1.80:9494', 'http://192.168.1.81:9494', 'http://192.168.1.82:9494']; // 
 
-export async function vpnAxiosRequest(config: AxiosRequestConfig, free?: boolean) {
-	const random = free ? VPNS_FREE[Math.floor(Math.random() * VPNS_FREE.length)] : VPNS[Math.floor(Math.random() * VPNS.length)];
+const vpnData = VPNS.map(ip => {
+	return { ip, lastused: Date.now() }
+});
+export async function vpnAxiosRequest(config: AxiosRequestConfig) {
+	// const random = free ? VPNS_FREE[Math.floor(Math.random() * VPNS_FREE.length)] : VPNS[Math.floor(Math.random() * VPNS.length)];
+
 	const response = await Axios({
 		method: 'POST',
-		url: `${random}/vpnrequest`,
+		url: `${getOldestUsed()}/vpnrequest`,
 		data: {
 			config
 		}
 	});
 	const parsedRes = response && response.data && JSON.parse(response.data.data);
 	return parsedRes;
+}
+function getOldestUsed(): string {
+	const sorted = vpnData.sort((a, b) => a.lastused - b.lastused);
+	sorted[0].lastused = Date.now();
+	return sorted[0].ip;
 }
