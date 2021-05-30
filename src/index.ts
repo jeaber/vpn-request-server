55a391af205bce20701605357cb7f78c13807584
Jeaber <jeaber@gmail.com>
2021-05-30 02:45:46 -0500

    forward axios error information

--- src/index.ts
+++ src/index.ts
@@ -25,11 +25,31 @@ app.post('/vpnrequest', async (req, res) => {
 		} else {
 			config = req.body.config;
 		}
-		const response: AxiosResponse = await Axios(config);
-		if (response)
-			res.status(200).send({ message: 'ok', data: circularJSON.stringify(response) });
-		else
-			res.status(500).send({ message: 'err' });
+		let response;
+		let error;
+		await Axios(config)
+			.then(r => {
+				response = r;
+			})
+			.catch((e: Error) => {
+				error = e;
+			});
+		if (response) {
+			// console.log('/vpnrequest success', !!error);
+			res.status(200).send({ message: 'ok', response: circularJSON.stringify(response) });
+		} else if (error) {
+			// console.log('/vpnrequest error', error);
+			// { "message": "Request failed with status code 404", "name": "Error", "stack": "Error: Request failed with status code 404\\n    at createError (/home/jeab/Projects/dayz-killfeed/node_modules/axios/lib/core/createError.js:16:15)\\n    at settle (/home/jeab/Projects/dayz-killfeed/node_modules/axios/lib/core/settle.js:17:12)\\n    at IncomingMessage.handleStreamEnd (/home/jeab/Projects/dayz-killfeed/node_modules/axios/lib/adapters/http.js:236:11)\\n    at IncomingMessage.emit (events.js:326:22)\\n    at endReadableNT (_stream_readable.js:1244:12)\\n    at processTicksAndRejections (internal/process/task_queues.js:80:21)", "config": { "url": "http://httpstat.us/404", "method": "get", "headers": { "Accept": "application/json, text/plain, */*", "User-Agent": "axios/0.19.2" }, "transformRequest": [null], "transformResponse": [null], "timeout": 0, "xsrfCookieName": "XSRF-TOKEN", "xsrfHeaderName": "X-XSRF-TOKEN", "maxContentLength": -1 } } '
+			res.status(200).send({
+				message: error.message,
+				name: error.name,
+				stack: error.stack,
+				config: error.config,
+				response: circularJSON.stringify(error.response)
+			});
+		} else {
+			res.status(500).send({ message: `Error no response` });
+		}
 	} else {
 		console.error('no req.body');
 	}
--- src/project_file.ts
+++ src/project_file.ts
@@ -1,22 +1,40 @@
 import Axios, { AxiosRequestConfig } from "axios";
-
+const timeAgo = require('node-time-ago');
 export const VPNS = ['http://192.168.1.90:9494', 'http://192.168.1.91:9494', 'http://192.168.1.92:9494', 'http://192.168.1.93:9494', 'http://192.168.1.80:9494', 'http://192.168.1.81:9494', 'http://192.168.1.82:9494']; // 
+// export const VPNS = ['http://localhost:9494'];
 
 const vpnData = VPNS.map(ip => {
 	return { ip, lastused: Date.now() }
 });
-export async function vpnAxiosRequest(config: AxiosRequestConfig) {
+export async function vpnAxiosRequest(config: AxiosRequestConfig): Promise<any> {
 	// const random = free ? VPNS_FREE[Math.floor(Math.random() * VPNS_FREE.length)] : VPNS[Math.floor(Math.random() * VPNS.length)];
-
-	const response = await Axios({
-		method: 'POST',
-		url: `${getOldestUsed()}/vpnrequest`,
-		data: {
-			config
-		}
-	});
-	const parsedRes = response && response.data && JSON.parse(response.data.data);
-	return parsedRes;
+	const vpn = getOldestUsed();
+	return new Promise(async (resolve, reject) => {
+		await Axios({
+			method: 'POST',
+			url: `${vpn}/vpnrequest`,
+			data: {
+				config
+			}
+		})
+			.then(res => {
+				// console.log('vpnAxiosRequest res1', res.data.message)
+				res.data.response = JSON.parse(res.data.response);
+				if (res.data.name === 'Error') {
+					console.log('---', res, '----')
+					// console.log('vpnAxiosRequest err res2', JSON.parse(res.data.data));
+					reject(res.data);
+				} else if (res.data.message === 'ok') {
+					// console.log('vpnAxiosRequest ok res2', res.data.data)
+					resolve(res && res.data && res.data.response);
+				}
+				// console.log('vpnAxiosRequest success')
+			})
+			.catch(err => {
+				reject(new Error(err));
+				// console.log('vpnAxiosRequest err', err)
+			});
+	})
 }
 function getOldestUsed(): string {
 	const sorted = vpnData.sort((a, b) => a.lastused - b.lastused);
