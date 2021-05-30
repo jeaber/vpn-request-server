cb0a18cb1dae947c726e92f6d8269e11e2b25998
Jeaber <jeaber@gmail.com>
2021-05-30 13:43:21 -0500

    ping vpns and filter offline out

--- src/project_file.ts
+++ src/project_file.ts
@@ -1,24 +1,36 @@
 import Axios, { AxiosRequestConfig } from "axios";
+import { interval } from "rxjs";
 const timeAgo = require('node-time-ago');
-export const VPNS = ['http://192.168.1.90:9494', 'http://192.168.1.91:9494', 'http://192.168.1.92:9494', 'http://192.168.1.93:9494', 'http://192.168.1.80:9494', 'http://192.168.1.81:9494', 'http://192.168.1.82:9494']; // 
-// export const VPNS = ['http://localhost:9494'];
-
+var ping = require('ping');
+export const VPNS = ['192.168.1.90', '192.168.1.91', '192.168.1.92', '192.168.1.93', '192.168.1.80', '192.168.1.81', '192.168.1.82']; // 
+// export const VPNS = ['localhost'];
 const vpnData = VPNS.map(ip => {
-	return { ip, lastused: Date.now() }
+	return { ip, lastused: Date.now(), alive: true }
 });
+
+interval(5000).subscribe(() => {
+	vpnData.forEach((host) => {
+		ping.sys.probe(host.ip, (isAlive) => {
+			var msg = isAlive ? 'host ' + host.ip + ' is alive' : 'host ' + host.ip + ' is dead';
+			host.alive = isAlive;
+			console.log(msg);
+		});
+	});
+});
+
 export async function vpnAxiosRequest(config: AxiosRequestConfig): Promise<any> {
 	// const random = free ? VPNS_FREE[Math.floor(Math.random() * VPNS_FREE.length)] : VPNS[Math.floor(Math.random() * VPNS.length)];
 	const vpn = getOldestUsed();
 	return new Promise(async (resolve, reject) => {
 		await Axios({
 			method: 'POST',
-			url: `${vpn}/vpnrequest`,
+			url: `http://${vpn}:9494/vpnrequest`,
 			data: {
 				config
 			}
 		})
 			.then(res => {
-				// console.log('vpnAxiosRequest res1', res.data.message)
+				console.log('vpnAxiosRequest res1', res.data.message)
 				res.data.response = JSON.parse(res.data.response);
 				if (res.data.name === 'Error') {
 					console.log('---', res, '----')
@@ -28,16 +40,16 @@ export async function vpnAxiosRequest(config: AxiosRequestConfig): Promise<any> 
 					// console.log('vpnAxiosRequest ok res2', res.data.data)
 					resolve(res && res.data && res.data.response);
 				}
-				// console.log('vpnAxiosRequest success')
+				console.log('vpnAxiosRequest success')
 			})
 			.catch(err => {
 				reject(new Error(err));
-				// console.log('vpnAxiosRequest err', err)
+				console.log('vpnAxiosRequest err', err)
 			});
 	})
 }
 function getOldestUsed(): string {
-	const sorted = vpnData.sort((a, b) => a.lastused - b.lastused);
+	const sorted = vpnData.filter((vpn) => vpn.alive).sort((a, b) => a.lastused - b.lastused);
 	sorted[0].lastused = Date.now();
 	return sorted[0].ip;
 }
