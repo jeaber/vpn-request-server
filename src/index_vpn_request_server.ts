97b289f575db68ab40b4b4e34f2fc4858d07ce13
Jeaber <jeaber@gmail.com>
2021-06-21 00:33:02 -0500

    not every json

--- src/index_vpn_request_server.ts
+++ src/index_vpn_request_server.ts
@@ -35,17 +35,29 @@ app.post('/vpnrequest', async (req, res) => {
 				error = e;
 			});
 		if (response) {
+			let res;
+			try {
+				res = circularJSON.stringify(response);
+			} catch (e) {
+				res = response;
+			}
 			// console.log('/vpnrequest success', !!error);
-			res.status(200).send({ message: 'ok', response: circularJSON.stringify(response) });
+			res.status(200).send({ message: 'ok', response: res });
 		} else if (error) {
 			// console.log('/vpnrequest error', error);
 			// { "message": "Request failed with status code 404", "name": "Error", "stack": "Error: Request failed with status code 404\\n    at createError (/home/jeab/Projects/dayz-killfeed/node_modules/axios/lib/core/createError.js:16:15)\\n    at settle (/home/jeab/Projects/dayz-killfeed/node_modules/axios/lib/core/settle.js:17:12)\\n    at IncomingMessage.handleStreamEnd (/home/jeab/Projects/dayz-killfeed/node_modules/axios/lib/adapters/http.js:236:11)\\n    at IncomingMessage.emit (events.js:326:22)\\n    at endReadableNT (_stream_readable.js:1244:12)\\n    at processTicksAndRejections (internal/process/task_queues.js:80:21)", "config": { "url": "http://httpstat.us/404", "method": "get", "headers": { "Accept": "application/json, text/plain, */*", "User-Agent": "axios/0.19.2" }, "transformRequest": [null], "transformResponse": [null], "timeout": 0, "xsrfCookieName": "XSRF-TOKEN", "xsrfHeaderName": "X-XSRF-TOKEN", "maxContentLength": -1 } } '
+			let res;
+			try {
+				res = circularJSON.stringify(error.response);
+			} catch (e) {
+				res = error.response;
+			}
 			res.status(200).send({
 				message: error.message,
 				name: error.name,
 				stack: error.stack,
 				config: error.config,
-				response: circularJSON.stringify(error.response)
+				response: res
 			});
 		} else {
 			res.status(500).send({ message: `Error no response` });
